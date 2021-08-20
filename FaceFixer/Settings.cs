b1fcde6f4ef39bf2b5181d1b7d140f7538951a57
Irwine <vandevelde632@gmail.com>
2021-08-21 00:35:44 +0200

    Update Settings.cs
    Traduction des options

--- FaceFixer/Settings.cs
+++ FaceFixer/Settings.cs
@@ -8,20 +8,20 @@ namespace FaceFixer
     public record Settings
     {
         public List<ModKey> TargetMods = new List<ModKey>();
-        public bool PrioritizeBySpecifiedOrder = false;
-        public bool PatchAttackRace = true;
-        public bool PatchDefaultOutfit = false;
-        public bool PatchFaceMorph = true;
-        public bool PatchFaceParts = true;
-        public bool PatchFarAwayModel = true;
-        public bool PatchHairColor = true;
-        public bool PatchHeadParts = true;
-        public bool PatchHeadTexture = true;
-        public bool PatchHeight = true;
-        public bool PatchSleepingOutfit = false;
-        public bool PatchTextureLighting = true;
-        public bool PatchTintLayers = true;
-        public bool PatchWeight = true;
-        public bool PatchWornArmor = true;
+        public bool PrioriseParLOrdreSpecifie = false;
+        public bool AttaqueRaciale = true;
+        public bool TenueParDefaut = false;
+        public bool MorphologieDuVisage = true;
+        public bool PartiesDuVisage = true;
+        public bool ModeleDistant = true;
+        public bool CouleurDeCheveux = true;
+        public bool PartiesDeLaTete = true;
+        public bool TextureDeLeTete = true;
+        public bool Taille = true;
+        public bool TenueDeNuit = false;
+        public bool EclairageTexture = true;
+        public bool Couches = true;
+        public bool Poids = true;
+        public bool ArmurePortee = true;
     }
 }
