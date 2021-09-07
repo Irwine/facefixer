983df43b90db1fd8ec117c95d34bf9a3048b7ed3
Irwine <vandevelde632@gmail.com>
2021-09-07 21:23:21 +0200

    Update Settings.cs

--- FaceFixer/Settings.cs
+++ FaceFixer/Settings.cs
@@ -1,4 +1,5 @@
 using Mutagen.Bethesda;
+using Mutagen.Bethesda.WPF.Reflection.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,21 +8,52 @@ namespace FaceFixer
 {
     public record Settings
     {
-        public List<ModKey> ModsCibles = new List<ModKey>();
-        public bool PrioriseParLOrdreSpecifie = false;
-        public bool AttaqueRaciale = true;
-        public bool TenueParDefaut = false;
-        public bool MorphologieDuVisage = true;
-        public bool PartiesDuVisage = true;
-        public bool ModeleDistant = true;
-        public bool CouleurDeCheveux = true;
-        public bool PartiesDeLaTete = true;
-        public bool TextureDeLeTete = true;
-        public bool Taille = true;
-        public bool TenueDeNuit = false;
-        public bool EclairageTexture = true;
-        public bool Couches = true;
-        public bool Poids = true;
+        [SettingName("Mods ciblés")]
+        public List<ModKey> TargetMods = new List<ModKey>();
+        
+        [SettingName("Prioriser par l'ordre spécifié")]
+        public bool PrioritizeBySpecifiedOrder = false;
+        
+        [SettingName("Attaque raciale")]
+        public bool PatchAttackRace = true;
+        
+        [SettingName("Tenue par défaut")]
+        public bool PatchDefaultOutfit = false;
+        
+        [SettingName("Morphologie du visage")]
+        public bool PatchFaceMorph = true;
+        
+        [SettingName("Parties du visage")]
+        public bool PatchFaceParts = true;
+        
+        [SettingName("Modèle distant")]
+        public bool PatchFarAwayModel = true;
+        
+        [SettingName("Couleur des cheveux")]
+        public bool PatchHairColor = true;
+        
+        [SettingName("Parties de la tête")]
+        public bool PatchHeadParts = true;
+        
+        [SettingName("Textures de la tête")]
+        public bool PatchHeadTexture = true;
+        
+        [SettingName("Taille")]
+        public bool PatchHeight = true;
+        
+        [SettingName("Tenue de nuit")]
+        public bool PatchSleepingOutfit = false;
+        
+        [SettingName("Luminosité des textures")]
+        public bool PatchTextureLighting = true;
+        
+        [SettingName("Nuances")]
+        public bool PatchTintLayers = true;
+        
+        [SettingName("Poids")]
+        public bool PatchWeight = true;
+        
+        [SettingName("Armure portée")]
         public bool ArmurePortee = true;
     }
 }
